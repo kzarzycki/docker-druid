a4cecd91b38d0a8a9ade294e2ec143318c3dc6f4
Darren Gruber <dgruber@gmail.com>
2014-12-13 21:24:25 -0500

    Set host to IP, refactor config changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,30 +1,38 @@
 #!/usr/bin/env bash
 set -x
 
-DRUID_ROLE=${1:-"coordinator"}
+DRUID_NODE_TYPE=${1:-"coordinator"}
 MEMORY=${JAVA_MEMORY:-"768m"}
 
+EXAMPLE_SPEC=${EXAMPLE:-"wikipedia"}
+SPEC="/druid/examples/${EXAMPLE_SPEC}/${EXAMPLE_SPEC}_realtime.spec"
+IP=$(ip addr | grep 'eth0' | awk '{print $2}' | cut -f1  -d'/' | tail -1)
+
+druid_config(){
+  sed -i "$1" /druid/config/$DRUID_NODE_TYPE/runtime.properties
+}
+
 if env | grep -q MYSQL_PORT_3306_TCP_ADDR; then
-  sed -i "s/druid.db.connector.connectURI=.*/druid.db.connector.connectURI=jdbc\\\:mysql\\\:\/\/$MYSQL_PORT_3306_TCP_ADDR\\\:3306\/druid/g" /druid/config/$DRUID_ROLE/runtime.properties
-  sed -i 's/\# druid.db.connector/druid.db.connector/g' /druid/config/$DRUID_ROLE/runtime.properties
+  druid_config "s/druid.db.connector.connectURI=.*/druid.db.connector.connectURI=jdbc\\\:mysql\\\:\/\/$MYSQL_PORT_3306_TCP_ADDR\\\:3306\/druid/g"
+  druid_config 's/\# druid.db.connector/druid.db.connector/g'
 fi
 
 if env | grep -q ZK_PORT_2181_TCP_ADDR; then
-  sed -i "s/druid.zk.service.host=localhost/druid.zk.service.host=$ZK_PORT_2181_TCP_ADDR/g" /druid/config/$DRUID_ROLE/runtime.properties
+  druid_config "s/druid.zk.service.host=localhost/druid.zk.service.host=$ZK_PORT_2181_TCP_ADDR/g"
 fi
 
 # Standardize port to 8000
-sed -i 's/druid.port=.*/druid.port=8000/g' /druid/config/$DRUID_ROLE/runtime.properties
+druid_config 's/druid.port=.*/druid.port=8000/g'
 
 # Set specific hostname
-sed -i "s/druid.host=.*/druid.host=$HOSTNAME/g" /druid/config/$DRUID_ROLE/runtime.properties
+druid_config "s/druid.host=.*/druid.host=$IP/g"
 
-if [ "$DRUID_ROLE" = "realtime" ]; then
-  sed -i "s/druid.publish.type=.*/druid.publish.type=db/g" /druid/config/$DRUID_ROLE/runtime.properties
-  echo "druid.realtime.specFile=/druid/examples/wikipedia/wikipedia_realtime.spec" >> /druid/config/$DRUID_ROLE/runtime.properties
+if [ "$DRUID_NODE_TYPE" = "realtime" ]; then
+  druid_config "s/druid.publish.type=.*/druid.publish.type=db/g"
+  echo "druid.realtime.specFile=$SPEC" >> /druid/config/$DRUID_NODE_TYPE/runtime.properties
 fi
 
-cat /druid/config/$DRUID_ROLE/runtime.properties
+cat /druid/config/$DRUID_NODE_TYPE/runtime.properties
 
 sleep 5
 
@@ -32,5 +40,5 @@ java -server \
      -Xmx$MEMORY \
      -Duser.timezone=UTC \
      -Dfile.encoding=UTF-8 \
-     -cp /druid/lib/*:/druid/config/$DRUID_ROLE \
-     io.druid.cli.Main server $DRUID_ROLE
+     -cp /druid/lib/*:/druid/config/$DRUID_NODE_TYPE \
+     io.druid.cli.Main server $DRUID_NODE_TYPE
